2f320ea43c9e128b2aa4cf28debe27fb41ee0ebc
Sandy May <sandy@elastacloud.com>
2018-02-01 22:22:25 +0000

    demo ready

--- demo.scala
+++ demo.scala
@@ -7,8 +7,6 @@ val dataframe = sc.parallelize(Array(MyCaseClass("f", "consonants", 1),
        MyCaseClass("j", "consonants", 5))
 ).toDF()
 
-//dataframe.show
-
 // COMMAND ----------
 
 display(dataframe)
@@ -37,3 +35,60 @@ val salesEntryDataFrame = sc.parallelize(
   SalesEntry("misc", "cleaning_supplies", 2015, 400.35) ::
   Nil).toDF()
 display(salesEntryDataFrame)
+
+// COMMAND ----------
+
+case class WorldEntry(country: String, value: Int)
+val world = sc.parallelize(
+  WorldEntry("USA", 1000) ::
+  WorldEntry("JPN", 23) ::
+  WorldEntry("GBR", 23) ::
+  WorldEntry("FRA", 21) ::
+  WorldEntry("TUR", 3) ::
+  Nil).toDF()
+display(world)
+
+// COMMAND ----------
+
+val rng = new scala.util.Random(0)
+val points = sc.parallelize((0L until 1000L).map { x => (x/100.0, 4 * math.sin(x/100.0) + rng.nextGaussian()) }).toDF()
+display(points)
+
+// COMMAND ----------
+
+// MAGIC %r
+// MAGIC library(ggplot2)
+// MAGIC ggplot(diamonds, aes(carat, price, color = color, group = 1)) + geom_point(alpha = 0.3) + stat_smooth()
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC from pyspark.sql import Row
+// MAGIC 
+// MAGIC array = map(lambda x: Row(key="k_%04d" % x, value = x), range(1, 5001))
+// MAGIC largeDataFrame = sqlContext.createDataFrame(sc.parallelize(array))
+// MAGIC display(largeDataFrame)
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC 
+// MAGIC from bokeh.plotting import figure
+// MAGIC from bokeh.embed import components, file_html
+// MAGIC from bokeh.resources import CDN
+// MAGIC 
+// MAGIC # prepare some data
+// MAGIC x = [1, 2, 3, 4, 5]
+// MAGIC y = [6, 7, 2, 4, 5]
+// MAGIC 
+// MAGIC # create a new plot with a title and axis labels
+// MAGIC p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+// MAGIC 
+// MAGIC # add a line renderer with legend and line thickness
+// MAGIC p.line(x, y, legend="Temp.", line_width=2)
+// MAGIC 
+// MAGIC # create an html document that embeds the Bokeh plot
+// MAGIC html = file_html(p, CDN, "my plot1")
+// MAGIC 
+// MAGIC # display this html
+// MAGIC displayHTML(html)
